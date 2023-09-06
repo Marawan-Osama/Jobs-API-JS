9bd2ce650d54aac886fcebaf904f1ef13f27ec1a
Marawan-Osama <marawanegy@gmail.com>
2023-09-06 23:18:31 +0300

    added password checking function in model

--- models/User.js
+++ models/User.js
@@ -34,6 +34,11 @@ UserSchema.methods.getName = function(){
     return this.name;
 }
 
+UserSchema.methods.comparePassword = async function(candidatePassword){
+    const isMatch = await bcrypt.compare(candidatePassword, this.password)
+    return isMatch
+}
+
 UserSchema.methods.createJWT = function(){
     return jwt.sign({userID:this._id,name:this.name}, process.env.JWT_SECRET,{expiresIn:process.env.JWT_LIFETIME})}
 
