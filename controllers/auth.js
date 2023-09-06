daf025d57c1ede089366c0574206eac334a2631a
Marawan-Osama <marawanegy@gmail.com>
2023-09-06 23:14:34 +0300

    Adjusted model and controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,29 @@
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
 const {BadRequestError} = require('../errors')
+const {UnauthenticatedError} = require('../errors')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const register = async (req, res) => {
 
     const user = await User.create({...req.body})
-    const token = jwt.sign({userId:user._id}, process.env.JWT_SECRET, {expiresIn:'30d'})
+    const token = user.createJWT()
     res.status(StatusCodes.CREATED).json({user:{name:user.name},token})
 }
 
 const login = async (req, res) => {
-    res.send('login user')
+    const {email, password} = req.body
+    if(!email || !password){
+        throw new BadRequestError('Please provide email and password')
+    }
+    const user = await User.findOne({email})
+    if(!user){
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+
+    const token = user.createJWT()
+    res.status(StatusCodes.OK).json({user:{name:user.name},token})
 }
 
 module.exports = {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 const UserSchema = new mongoose.Schema({
     name:{
@@ -27,4 +29,12 @@ UserSchema.pre('save', async function(){
     const salt = await bcrypt.genSalt(10)
     this.password = await bcrypt.hash(this.password, salt)
 })
+
+UserSchema.methods.getName = function(){
+    return this.name;
+}
+
+UserSchema.methods.createJWT = function(){
+    return jwt.sign({userID:this._id,name:this.name}, process.env.JWT_SECRET,{expiresIn:process.env.JWT_LIFETIME})}
+
 module.exports = mongoose.model('User',UserSchema);
