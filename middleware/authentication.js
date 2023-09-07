a9349a5ebc66cca58128d85c31bba1a5b30f5d52
Marawan-Osama <marawanegy@gmail.com>
2023-09-07 18:53:06 +0300

    adjusted auth middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,27 @@
+const User = require('../models/User')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError} = require('../errors')
+const {UnauthenticatedError} = require('../errors')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const auth = async (req, res, next) => {
+
+    //check header
+    const authHeader = req.headers.authorization
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        throw new UnauthenticatedError('Authentication invalid')
+    }
+    const token = authHeader.split(' ')[1]
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+        //attach user to job route
+        req.user = {userID:payload.userID,name:payload.name}
+    }
+    catch (error) {
+        throw new UnauthenticatedError('Authentication invalid')
+    }
+}
+
+module.exports = auth
+
