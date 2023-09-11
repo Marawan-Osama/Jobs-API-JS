af9a990d0985a69165c942e86b366e3572aff61d
Marawan-Osama <marawanegy@gmail.com>
2023-09-11 18:12:10 +0300

    added some comments for further clarification

--- models/User.js
+++ models/User.js
@@ -14,6 +14,7 @@ const UserSchema = new mongoose.Schema({
         type:String,
         required:[true,'Please provide email'],
         unique:true,
+        //regex addition for email validation
         match:[/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         ,'Please provide a valid email']
     },
